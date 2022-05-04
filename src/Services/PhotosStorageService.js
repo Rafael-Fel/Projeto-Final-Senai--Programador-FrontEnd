69fa35f80feb037161efd4c3c80c57815fe47720
Rafael Geisel <rafaelgeisel@gmail.com>
2022-05-04 18:29:47 -0300

    Pequenas correçoes, nenhuma alteração

--- src/Services/PhotosStorageService.js
+++ src/Services/PhotosStorageService.js
@@ -2,17 +2,18 @@ import axios from "axios";
 
 export default class PhotosStorageService {
   constructor() { }
-  
-  
+
+
   BASE_URL = "https://senaigram.herokuapp.com";
-  
-  
+
+
 
   getList(link) {
     return axios
       .get(`${this.BASE_URL}${link}`)
       .then((response) => {
         return response.data;
+
       })
       .catch((error) => {
         console.log(error);
